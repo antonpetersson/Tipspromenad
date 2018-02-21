637d4fb6693ff6d22641620c5b7c4e4ab36489a0
antonpetersson <antonpetersson@users.noreply.github.com>
2018-02-21 16:02:11 +0100

    Fixat lite PHP-CSS

--- classes.php
+++ classes.php
@@ -15,30 +15,55 @@ class Question{
         $this -> correct = $correct;
     }
     function printQuestion(){
+        
+        if(isset($_POST['options' . $this->id])) {
+            $right = false;
+            if ($_POST['options' . $this->id] == $this->correct){
+                $right = true;
+            }
+        }
+
         echo '<tr><th id="thQuestion' . $this->id . '" rowspan="3">' . $this->text . '</th>';
-        echo '<td>';
+
+        if(isset($right)) {
+            if($this->correct == 'answer1') echo '<td style="background-color: #2ECC40;">';
+            else echo '<td>';
+        } else echo '<td>';
             echo '1 <input type="radio" name="options' . $this->id . '" value="answer1"';
             if (isset($_POST['options' . $this->id]) && $_POST['options' . $this->id] == 'answer1'){echo ' checked="checked"';}
             echo '>' . $this->answer1;
-        echo '</td></tr><tr><td>';
+        echo '</td></tr><tr>';
+
+        if(isset($right)) {
+            if($this->correct == 'answerX') echo '<td style="background-color: #2ECC40;">';
+            else echo '<td>';
+        } else echo '<td>';
             echo 'X<input type="radio" name="options' . $this->id . '" value="answerX"';
             if (isset($_POST['options' . $this->id]) && $_POST['options' . $this->id] == 'answerX'){echo ' checked="checked"';}
             echo '>' . $this->answerX;
-        echo '</td></tr><tr><td>';
+        echo '</td></tr><tr>';
+
+        if(isset($right)) {
+            if($this->correct == 'answer2') echo '<td style="background-color: #2ECC40;">';
+            else echo '<td>';
+        } else echo '<td>';
             echo '2 <input type="radio" name="options' . $this->id . '" value="answer2"';
             if (isset($_POST['options' . $this->id]) && $_POST['options' . $this->id] == 'answer2'){echo ' checked="checked"';}
             echo '>' . $this->answer2;
         echo '</td></tr>';
 
-        if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+        if(isset($_POST['options' . $this->id])) {
             // $_SESSION = $_POST;
             if ($_POST['options' . $this->id] == $this->correct){
-                echo '<style>#thQuestion' . $this->id . ' {background-color: green;}</style>';
+                echo '<style>#thQuestion' . $this->id . ' {background-color: #2ECC40;}</style>';
                 $GLOBALS["numCorrectAnswers"] ++;
             }
             else{
-                echo '<style>#thQuestion' . $this->id . ' {background-color: red;}</style>';
+                echo '<style>#thQuestion' . $this->id . ' {background-color: #FF4136 ;}</style>';
+              
             }
+
+            
             
         }
  
