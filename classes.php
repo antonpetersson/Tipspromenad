c09b6167beb7683ca317c27da7faad2f0735c5d6
antonpetersson <antonpetersson@users.noreply.github.com>
2018-02-21 15:18:13 +0100

    svarsalternativen sitter kvar.

--- classes.php
+++ classes.php
@@ -17,11 +17,17 @@ class Question{
     function printQuestion(){
         echo '<tr><th id="thQuestion' . $this->id . '" rowspan="3">' . $this->text . '</th>';
         echo '<td>';
-        echo '1 <input type="radio" name="options' . $this->id . '" value="answer1">' . $this->answer1;
+            echo '1 <input type="radio" name="options' . $this->id . '" value="answer1"';
+            if (isset($_POST['options' . $this->id]) && $_POST['options' . $this->id] == 'answer1'){echo ' checked="checked"';}
+            echo '>' . $this->answer1;
         echo '</td></tr><tr><td>';
-        echo 'X<input type="radio" name="options' . $this->id . '" value="answerX">' . $this->answerX;
+            echo 'X<input type="radio" name="options' . $this->id . '" value="answerX"';
+            if (isset($_POST['options' . $this->id]) && $_POST['options' . $this->id] == 'answerX'){echo ' checked="checked"';}
+            echo '>' . $this->answerX;
         echo '</td></tr><tr><td>';
-        echo '2 <input type="radio" name="options' . $this->id . '" value="answer2">' . $this->answer2;
+            echo '2 <input type="radio" name="options' . $this->id . '" value="answer2"';
+            if (isset($_POST['options' . $this->id]) && $_POST['options' . $this->id] == 'answer2'){echo ' checked="checked"';}
+            echo '>' . $this->answer2;
         echo '</td></tr>';
 
         if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
